218a8399e9717de40db49594085850d8d8a027c5
Lorenzo Bersano <lorenzo.bersano@gmail.com>
2020-05-14 22:54:41 +0200

    Put url as documentTitle in analytics as of now

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Layout from '../components/Layout'
 Router.events.on('routeChangeComplete', (url) => {
   if (window && window._paq) {
     window._paq.push(['setCustomUrl', url])
-    window._paq.push(['setDocumentTitle', document.title])
+    window._paq.push(['setDocumentTitle', url])
     window._paq.push(['trackPageView'])
   }
 })
